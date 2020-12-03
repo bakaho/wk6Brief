16428db24cf1e9c723666eff7ac0360d94b8e61b
bakaho <cleariqd@gmail.com>
2020-12-03 15:36:49 +0000

    UpdateCamera

---
+++ brief6/Assets/Script/cameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    float offset;
+    public Transform mainCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeFollow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, mainCharacter.position.y + offset, -10);
+    }
+
+    void ChangeFollow()
+    {
+        offset = transform.position.y - mainCharacter.position.y;
+    }
+}
